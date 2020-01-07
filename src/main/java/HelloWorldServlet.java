fa2f82b192e8af7ff1d34df799a91ed2e919ad15
Roger Mayer <rmayer1984@gmail.com>
2020-01-07 12:59:44 -0600

    completed servlets ex

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,32 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    int hitCount = 0;
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String name = req.getParameter("name");
+        if (name == null || name.equals("")) {
+            res.setContentType("text/html");
+            hitCount++;
+            PrintWriter pw = res.getWriter();
+            pw.println("<h1>Hello, World!</h1>");
+            pw.println("<h1>Hitcount is " + hitCount + "!</h1>");
+        } else if (name.equalsIgnoreCase("resetcounter")){
+            res.setContentType("text/html");
+            hitCount = 0;
+            PrintWriter pw = res.getWriter();
+            pw.println("<h1>Hello, World!</h1>");
+            pw.println("<h1>Hitcount is " + hitCount + "!</h1>");
+        } else {
+            res.setContentType("text/html");
+            hitCount++;
+            PrintWriter pw = res.getWriter();
+            pw.println("<h1>Hello, " + name + "!</h1>");
+            pw.println("<h1>Hitcount is " + hitCount + "!</h1>");
+        }
     }
 }
