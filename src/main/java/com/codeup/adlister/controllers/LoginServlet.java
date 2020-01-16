e07d99d574553ac617b41867454711e9f5252a29
Roger Mayer <rmayer1984@gmail.com>
2020-01-16 16:30:34 -0600

    completed ex

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -22,17 +22,22 @@ public class LoginServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String username = request.getParameter("username");
+        String username = request.getParameter("username"); //receives user input from login.jsp form
         String password = request.getParameter("password");
+
+        //find by username establishes that user name does exists
         User user = DaoFactory.getUsersDao().findByUsername(username);
 
+        //if does not exist, redirects back to login
         if (user == null) {
             response.sendRedirect("/login");
             return;
         }
 
+        //valid attempt is made true if user does exists
         boolean validAttempt = Password.check(password, user.getPassword());
 
+        //if attempt is valid user object is set as a session and sent to profile page
         if (validAttempt) {
             request.getSession().setAttribute("user", user);
             response.sendRedirect("/profile");
